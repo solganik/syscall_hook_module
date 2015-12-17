c4a841ed89de24766d48fcc1715dd16d84c6be7e
Alexander Solganik <sasha@stratoscale.com>
2015-12-17 19:51:54 +0200

    bugfix: first allocate table then hook the callback.
    This is in order to allow not to lock hashes, as we will only use them
    when everything is ready.

--- open_hack/main.c
+++ open_hack/main.c
@@ -10,6 +10,10 @@ asmlinkage int (*original_call)(const char *, int, int);
 asmlinkage int our_sys_open(const char *filename, int flags, int mode)
 {
 	TRACE_INFO("Redirecting to original %s", filename);
+	if (is_restricted(restricted_ctx,filename)){
+		TRACE_WARNING("File %s is restricted .. deny", filename);
+		return -EPERM;
+	}
 	return original_call(filename, flags, mode);
 }
 
@@ -17,17 +21,12 @@ static int __init hook_init(void)
 {
 	int ret = 0;
 	TRACE_INFO("Loading module");
-	original_call = hook_open(our_sys_open);
-	if (!original_call) {
-		TRACE_ERROR("Failed hook open syscall");
-		return -1; /* Donno what went wrong :) */
-	}
 
 	restricted_ctx = restricted_allocate();
 	if (!restricted_ctx) {
 		TRACE_ERROR("Failed to allocated restricted context");
 		ret = -ENOMEM;
-		goto unhook;
+		goto done;
 	}
 
 	ret = restricted_init(RESTRICTED_FILE_PATH, restricted_ctx);
@@ -35,19 +34,24 @@ static int __init hook_init(void)
 		TRACE_ERROR("Failed to initialize restricted ctx %p err=%d", restricted_ctx, ret);
 		goto free_ctx;
 	}
+
+	original_call = hook_open(our_sys_open);
+	if (!original_call) {
+		TRACE_ERROR("Failed hook open syscall");
+		goto free_ctx;
+	}
 	goto done;
+
 free_ctx:
 	restricted_destroy(restricted_ctx);
-unhook:
-	unhook_open(original_call);
 done:
 	return ret;
 }
 
 static void __exit hook_exit(void){
 	TRACE_DEBUG("Exiting");
-	restricted_destroy(restricted_ctx);
 	unhook_open(original_call);
+	restricted_destroy(restricted_ctx);
 	TRACE_INFO("Exit Done");
 }
 
